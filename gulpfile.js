b119738089fe5bf75753e4660ee699092feb1de0
[200~ <maievskiyandrew@gmail.com>
2022-02-06 14:28:06 +0200

    added new-items

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,8 @@ let gulp = require('gulp'),
         return gulp.src([
             'node_modules//normalize.css/normalize.css',
             'node_modules/slick-carousel/slick/slick.css',
-            'node_modules/magnific-popup/dist/magnific-popup.css'
+            'node_modules/magnific-popup/dist/magnific-popup.css',
+            'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
         ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -23,7 +24,8 @@ let gulp = require('gulp'),
         return gulp.src([
             'node_modules/slick-carousel/slick/slick.js',
             'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-            'node_modules/mixitup/dist/mixitup.js'
+            'node_modules/mixitup/dist/mixitup.js',
+            'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
         ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
