f9a8f497486a86dec550b28d4e42534f250094d7
[200~ <maievskiyandrew@gmail.com>
2022-02-23 19:43:15 +0200

    added withdrawals

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,9 @@ let gulp = require('gulp'),
             'node_modules/slick-carousel/slick/slick.css',
             'node_modules/magnific-popup/dist/magnific-popup.css',
             'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
-            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
+            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
+            'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+            'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css'
         ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -27,7 +29,8 @@ let gulp = require('gulp'),
             'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
             'node_modules/mixitup/dist/mixitup.js',
             'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
-            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
+            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+            'node_modules/jquery-form-styler/dist/jquery.formstyler.js'
         ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
